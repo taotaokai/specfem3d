2e205e680bb700b527074c3d70d88f4ea22eb385
Kai Tao <ktao@login1.stampede2.tacc.utexas.edu>
2018-10-11 05:04:58 -0500

    rename random_select_stations_with_include_or_exclude.py

--- utils_ktao/structure_inversion/random_select_stations.sh
+++ utils_ktao/structure_inversion/random_select_stations.sh
@@ -14,3 +14,14 @@ $sem_utils_dir/structure_inversion/random_select_stations.py \
 
 awk '{printf "%s.%s\n",$1,$2}' STATIONS.REF_ENU.subset > grep.f
 grep -f grep.f $data_dir/data/event.txt > ${outfile}
+
+$sem_utils_dir/structure_inversion/random_select_stations.py \
+  STATIONS.REF_ENU.subset ${minimum_inter_station_distance_for_common_set} \
+  "STATIONS.REF_ENU.subset.include" "STATIONS.REF_ENU.subset.include.pdf"
+
+grep -vF -f STATIONS.REF_ENU.subset.include STATIONS.REF_ENU.subset > STATIONS.REF_ENU.subset.exclude 
+
+$sem_utils_dir/structure_inversion/random_select_stations_with_include_and_exclude.py \
+  $data_dir/data/STATIONS.REF_ENU STATIONS.REF_ENU.subset.include STATIONS.REF_ENU.subset.exclude \
+  ${minimum_inter_station_distance} \
+  "STATIONS.REF_ENU.subset.new" "STATIONS.REF_ENU.subset.new.pdf"
